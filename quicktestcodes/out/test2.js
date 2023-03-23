aa73db1420c160c98e96ff0f08704c81bf2393e4
Isabelle Wilhelm <m.isabelle.wilhelm@gmail.com>
2023-03-23 10:41:38 +0100

    .

--- quicktestcodes/out/test2.js
+++
@@ -1,5 +0,0 @@
-function a() {
-    return [1, undefined];
-}
-var _a = a(), x = _a[0], y = _a[1];
-//# sourceMappingURL=test2.js.map
--- quicktestcodes/test2.ts
+++
@@ -1,6 +0,0 @@
-function a() {
-    return [1, undefined]
-}
-
-var [x, y] = a()
-
